ef0210de94f859a18369e0031ac7c29fc32fffd1
Tom Woehrle <thomas.e.woehrle@gmail.com>
2024-03-20 15:03:47 -0500

    Adjust inner loop and omniglot net

---
+++ alt_omniglot_net.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from collections import OrderedDict
+
+# Adapted from https://github.com/katerakelly/pytorch-maml
+
+
+class OmniglotNet(nn.Module):
+    '''
+    The base model for few-shot learning on Omniglot
+    '''
+
+    def __init__(self, num_classes):
+        super(OmniglotNet, self).__init__()
+        # Define the network
+        self.net = nn.Sequential(OrderedDict([
+            ('conv1', nn.Conv2d(3, 64, 3)),
+            ('bn1', nn.BatchNorm2d(64, momentum=1, affine=True)),
+            ('relu1', nn.ReLU(inplace=True)),
+            ('pool1', nn.MaxPool2d(2, 2)),
+            ('conv2', nn.Conv2d(64, 64, 3)),
+            ('bn2', nn.BatchNorm2d(64, momentum=1, affine=True)),
+            ('relu2', nn.ReLU(inplace=True)),
+            ('pool2', nn.MaxPool2d(2, 2)),
+            ('conv3', nn.Conv2d(64, 64, 3)),
+            ('bn3', nn.BatchNorm2d(64, momentum=1, affine=True)),
+            ('relu3', nn.ReLU(inplace=True)),
+            ('pool3', nn.MaxPool2d(2, 2)),
+            ('flatten', nn.Flatten()),
+            ('fc', nn.Linear(64, num_classes))
+        ]))
+
+        # Initialize weights
+        # self._init_weights()
+
+    def forward(self, x, weights=None):
+        ''' Define what happens to data in the net '''
+        if weights == None:
+            return self.net(x)
+        else:
+            # print(weights['net.fc.weight'].shape)
+            # print(weights['net.fc.bias'].shape)
+            # TODO add meaningful running mean and var?
+            running_mean = torch.zeros(64)
+            running_var = torch.ones(64)
+            x = F.conv2d(x, weights['net.conv1.weight'],
+                         weights['net.conv1.bias'])
+            x = F.batch_norm(x, running_mean=running_mean, running_var=running_var, weight=weights['net.bn1.weight'],
+                             bias=weights['net.bn1.bias'], momentum=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, kernel_size=2, stride=2)
+            x = F.conv2d(x, weights['net.conv2.weight'],
+                         weights['net.conv2.bias'])
+            x = F.batch_norm(x, running_mean=running_mean, running_var=running_var, weight=weights['net.bn2.weight'],
+                             bias=weights['net.bn2.bias'], momentum=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, kernel_size=2, stride=2)
+            x = F.conv2d(x, weights['net.conv3.weight'],
+                         weights['net.conv3.bias'])
+            x = F.batch_norm(x, running_mean=running_mean, running_var=running_var, weight=weights['net.bn3.weight'],
+                             bias=weights['net.bn3.bias'], momentum=1)
+            x = F.relu(x)
+            x = F.max_pool2d(x, kernel_size=2, stride=2)
+            x = torch.squeeze(x)
+            x = F.linear(x, weights['net.fc.weight'], weights['net.fc.bias'])
+            return x
--- maml.py
+++ maml.py
@@ -6,8 +6,9 @@ import sys
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from learn2learn import clone_module
-from omniglot_helper import get_all_chars
+from alt_omniglot_net import OmniglotNet as AltOmniglotNet
+from collections import OrderedDict
+from omniglot_helper import get_all_chars, test_sync
 from omniglot_net import OmniglotNet
 from torchvision import transforms
 from typing import List
@@ -55,11 +56,11 @@ n_episodes = 100
 meta_batch_size = 32
 n = 5
 k = 1
-alpha, beta = 0.4, 0.001  # learning rates during training
+alpha, beta = 0.4, 0.01  # learning rates during training
 # TODO find out real beta
 
 criterion = nn.CrossEntropyLoss(reduction='sum')  # same for every task
-meta_model = OmniglotNet(n)
+meta_model = AltOmniglotNet(n)
 meta_optimizer = optim.SGD(meta_model.parameters(), lr=beta)
 
 for i in range(n_episodes):
@@ -71,6 +72,7 @@ for i in range(n_episodes):
         # technically, get_task and generate_k_samples_from_task could easily be put into one function. However,
         # this approach sticks closer to the original concept of a task that generates samples
 
+        """
         task_model = clone_module(meta_model)  # not sure if this cloning works
         task_optimizer = optim.SGD(task_model.parameters(), lr=alpha)
         loss = criterion(task_model(x), y)
@@ -79,6 +81,34 @@ for i in range(n_episodes):
         test_task = get_task('val', n)
         x, y = generate_k_samples_from_task(test_task, k)
         meta_loss += criterion(task_model(x), y)
+        """
+
+        # inspiration taken from https://github.com/katerakelly/pytorch-maml/blob/master/src/inner_loop.py
+        meta_optimizer.zero_grad()
+        loss = criterion(meta_model.forward(x), y)
+        loss.backward()
+        # NOTE slightly different architecture needed if more than one update is made
+        task_theta = OrderedDict((name, param - alpha*param.grad)
+                                 for (name, param) in meta_model.named_parameters())
+
+        val_task = get_task('val', n)
+        x_val, y_val = generate_k_samples_from_task(val_task, k)
+        logits = meta_model.forward(x_val, weights=task_theta)
+        x_hat = torch.argmax(logits, dim=1)
+        # if i < 0:
+        #     for img, (idx, cls_hat), cls in zip(x_val, enumerate(x_hat), y_val):
+        #         img = img.permute(1, 2, 0)
+        #         img = img.numpy()
+        #         plt.imshow(img)
+        #         plt.title('logits:' + str(logits[idx]) + ' \nargmax:' +
+        #                   str(cls_hat.item()) + '\nreal:' + str(cls.item()))
+        #         plt.tight_layout()
+        #         plt.show()
+        # print(torch.argmax(x_hat, dim=1).shape)
+
+        task_val_loss = criterion(logits, y_val)
+        # print(task_val_loss)
+        meta_loss += task_val_loss
 
     print(i)
     print(meta_loss)
