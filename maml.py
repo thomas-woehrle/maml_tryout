6860db4c3537048d3ed602e7ef28724871872fea
Tom Woehrle <thomas.e.woehrle@gmail.com>
2024-05-06 12:26:36 -0500

    Add first version of multiple_inner_steps and 20way1shot config

--- maml.py
+++ maml.py
@@ -39,24 +39,28 @@ def inner_loop_update_for_testing(anil, model: maml_api.MamlModel, params, buffe
 
 
 def inner_loop_update(use_anil: bool, current_ep: int, model: maml_api.MamlModel, params, buffers, task: maml_api.MamlTask, alpha: float, inner_gradient_steps: int):
-    inner_gradient_steps = 1  # NOTE assumption for now
+    # inner_gradient_steps = 1  # NOTE assumption for now
     mode = maml_api.SampleMode.SUPPORT
     x_support, y_support = task.sample(mode, current_ep)
-    x_hat = model.func_forward(x_support, params, buffers)
-    train_loss = task.calc_loss(x_hat, y_support, mode, current_ep)
-    if use_anil:
-        head = {n: p for n, p in params.items() if n.startswith('head')
-                }  # NOTE assumes that head is assigned via self.head = ...
-        head_grads = autograd.grad(
-            train_loss, head.values(), create_graph=True)
-        # create_graph=True should enable second order, also leads to slower execution
-        head_i = {n: p - alpha *
-                  g for (n, p), g in zip(head.items(), head_grads)}
-        params_i = {**params, **head_i}
-    else:
-        grads = autograd.grad(train_loss, params.values(), create_graph=True)
-        params_i = {n: p - alpha *
-                    g for (n, p), g in zip(params.items(), grads)}
+    params_i = {n: p for n, p in params.items()}
+    for _ in range(inner_gradient_steps):
+        y_hat = model.func_forward(x_support, params_i, buffers)
+        train_loss = task.calc_loss(y_hat, y_support, mode, current_ep)
+        if use_anil:
+            head = {n: p for n, p in params_i.items() if n.startswith('head')
+                    }  # assumes that head is assigned via self.head = ...
+            head_grads = autograd.grad(
+                train_loss, head.values(), create_graph=True)
+            # create_graph=True enables second order, also leads to slower execution
+            head_i = {n: p - alpha *
+                      g for (n, p), g in zip(head.items(), head_grads)}
+            params_i = {**params_i, **head_i}
+        else:
+            grads = autograd.grad(
+                train_loss, params_i.values(), create_graph=True)
+
+            params_i = {n: p - alpha *
+                        g for (n, p), g in zip(params_i.items(), grads)}
     return params_i
 
 
@@ -81,13 +85,13 @@ def train(hparams: maml_config.MamlHyperParameters,
         acc_loss = 0.0  # Accumulated loss -> will become tensor
 
         for i in range(hparams.meta_batch_size):
-            mode = maml_api.SampleMode.QUERY
             task = sample_task()
 
             params_i = inner_loop_update(hparams.use_anil, episode, model,
                                          params, buffers, task, hparams.alpha, hparams.inner_gradient_steps)
 
             # Meta update
+            mode = maml_api.SampleMode.QUERY
             x_query, y_query = task.sample(mode, episode)
             test_loss = task.calc_loss(
                 model.func_forward(x_query, params_i, buffers), y_query, mode, episode)
