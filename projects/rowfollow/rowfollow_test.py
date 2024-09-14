9c0e642f28c044d66e9dfbaa4bd243837a80c3ca
Tom Woehrle <thomas.e.woehrle@gmail.com>
2024-09-14 16:05:02 +0200

    Change to test execution from .json file

--- projects/rowfollow/rowfollow_test.py
+++ projects/rowfollow/rowfollow_test.py
@@ -1,5 +1,8 @@
+import json
 import os
+import sys
 from dataclasses import dataclass
+from typing import Optional
 
 import mlflow
 import pandas as pd
@@ -11,18 +14,6 @@ import maml_eval
 
 import rowfollow_task
 import rowfollow_utils
-
-"""
-test config:
-model + [inner_buffers, inner_lrs] :
-    run_id
-    episode
-k
-inner_steps
-lr strategy
-support collection
-validation collections
-"""
 
 
 class RowfollowValDataset(torch.utils.data.Dataset):
@@ -82,19 +73,6 @@ class RowfollowValDataset(torch.utils.data.Dataset):
         return pre_processed_image, torch.stack([vp_gt, ll_gt, lr_gt])
 
 
-@dataclass
-class TestConfig:
-    run_id: str
-    episode: int
-    k: int
-    inner_steps: int
-    support_collection_path: str
-    support_annotations_file_path: str
-    validation_collections_paths: list[str]
-    validation_annotations_file_path: str
-    device: torch.device
-
-
 MLFLOW_CACHE_DIR = 'mlflow-cache/'
 
 
@@ -132,25 +110,32 @@ def load_inner_buffers(run_id: str, episode: int) -> maml_api.InnerBuffers:
     return inner_buffers
 
 
-def test_main(test_config: TestConfig):
-    model = load_model(test_config.run_id, test_config.episode)
-    inner_lrs = load_inner_lrs(test_config.run_id, test_config.episode)
-    inner_buffers = load_inner_buffers(test_config.run_id, test_config.episode)
+def test_main(run_id: str,
+              episode: int,
+              k: int,
+              inner_steps: int,
+              support_collection_path: str,
+              support_annotations_file_path: str,
+              validation_collections_paths: list[str],
+              validation_annotations_file_path: str,
+              device: torch.device,
+              seed: Optional[int]):
+    model = load_model(run_id, episode)
+    inner_lrs = load_inner_lrs(run_id, episode)
+    inner_buffers = load_inner_buffers(run_id, episode)
 
-    # TODO seed the finetuning
-    # TODO save the resulting model
-    task = rowfollow_task.RowfollowTaskOldDataset(test_config.support_annotations_file_path,
-                                                  test_config.support_collection_path,
-                                                  test_config.k,
-                                                  test_config.device,
-                                                  seed=0)
+    task = rowfollow_task.RowfollowTaskOldDataset(support_annotations_file_path,
+                                                  support_collection_path,
+                                                  k,
+                                                  device,
+                                                  seed=seed)
 
-    finetuner = maml_eval.MamlFinetuner(model, inner_lrs, inner_buffers, test_config.inner_steps, task)
+    finetuner = maml_eval.MamlFinetuner(model, inner_lrs, inner_buffers, inner_steps, task)
     finetuner.finetune()
 
     model.eval()
-    val_dataset = RowfollowValDataset(test_config.validation_collections_paths,
-                                      test_config.validation_annotations_file_path)
+    val_dataset = RowfollowValDataset(validation_collections_paths,
+                                      validation_annotations_file_path)
     val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=8, shuffle=False)
     total_loss = 0.0
     batches_processed = 0
@@ -164,24 +149,63 @@ def test_main(test_config: TestConfig):
     print('total loss: {}'.format(total_loss))
 
 
+@dataclass
+class TestConfig:
+    run_id: str
+    episode: int
+    k: int
+    inner_steps: int
+    base_path: str
+    support_collection_id: str
+    support_annotations_file_path: str
+    validation_collections_id: str
+    validation_annotations_file_path: str
+    seed: Optional[int]
+    device: torch.device
+
+
+def get_support_collection_path(base_path: str, support_collection_id: str) -> str:
+    # TODO Integrate more complicated ids
+    return os.path.join(base_path, 'train', support_collection_id)
+
+
+def get_validation_collections_paths(base_path: str, validation_collections_id: str) -> list[str]:
+    val_dir_base_path = os.path.join(base_path, 'val')
+    if validation_collections_id == 'all_val':
+        return [os.path.join(val_dir_base_path, d) for d in os.listdir(val_dir_base_path)
+                if os.path.isdir(os.path.join(val_dir_base_path, d))]
+    # TODO add other options
+
+
+def run_main_from_test_config(test_config: TestConfig):
+    test_main(run_id=test_config.run_id,
+              episode=test_config.episode,
+              k=test_config.k,
+              inner_steps=test_config.inner_steps,
+              support_collection_path=get_support_collection_path(test_config.base_path,
+                                                                  test_config.support_collection_id),
+              support_annotations_file_path=os.path.join(test_config.base_path,
+                                                         test_config.support_annotations_file_path),
+              validation_collections_paths=get_validation_collections_paths(test_config.base_path,
+                                                                            test_config.validation_collections_id),
+              validation_annotations_file_path=os.path.join(test_config.base_path,
+                                                            test_config.validation_annotations_file_path),
+              device=test_config.device,
+              seed=test_config.seed)
+
+
+def get_config_from_file(path: str) -> TestConfig:
+    with open(path, 'r') as f:
+        config_dict = json.load(f)
+        test_config = TestConfig(**config_dict)
+        test_config.device = torch.device(test_config.device)
+        return test_config
+
+
 if __name__ == '__main__':
-    base_path = '/Users/tomwoehrle/Documents/research_assistance/evaluate_adaptation/vision_data_latest/'
-    base_val_dir_path = os.path.join(base_path, 'val')
-    base_train_dir_path = os.path.join(base_path, 'train')
+    if len(sys.argv) != 2 or sys.argv[1] == '-h' or not os.path.exists(sys.argv[1]):
+        print('USAGE: python rowfollow_test.py path/to/test_config.json')
+        sys.exit(1)
 
-    all_val_collections = [os.path.join(base_val_dir_path, d) for d in os.listdir(base_val_dir_path)
-                           if os.path.isdir(os.path.join(base_val_dir_path, d))]
-
-    config = TestConfig(
-        run_id='af946f76673142368f2414f00259e51e',
-        episode=9999,
-        k=4,
-        inner_steps=3,
-        support_collection_path=os.path.join(base_train_dir_path, 'collection-150620'),
-        support_annotations_file_path=os.path.join(base_train_dir_path, 'v2_annotations_train.csv'),
-        validation_collections_paths=all_val_collections,
-        validation_annotations_file_path=os.path.join(base_val_dir_path, 'v2_annotations_val.csv'),
-        device=torch.device('cpu')
-    )
-
-    test_main(config)
+    path_to_config = sys.argv[1]
+    run_main_from_test_config(get_config_from_file(path_to_config))
